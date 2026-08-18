baf841a3f4920e3f78911b1756b652bd786310fe
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-08-18 21:07:01 +0530

    add utility method to check whether collection is null or empty

--- src/main/java/com/ecom/product/util/CommonUtil.java
+++ src/main/java/com/ecom/product/util/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.ecom.product.util;
 
+import java.util.Collection;
+
 public class CommonUtil {
 
 	public static boolean isBlank(String str) {
@@ -8,5 +10,9 @@ public class CommonUtil {
 	
 	public static boolean isNotBlank(String str) {
 		return str != null && !str.isBlank();
+	}
+	
+	public static boolean isEmpty(Collection<?> collection) {
+		return collection == null || collection.isEmpty();
 	}
 }
