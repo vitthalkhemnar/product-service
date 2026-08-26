ea08e395060202089e8bbc109fd05aa8a7c1d4fa
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-08-27 00:34:14 +0530

    expose product and variant to all cross origins

--- src/main/java/com/ecom/product/controller/ProductController.java
+++ src/main/java/com/ecom/product/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductController {
 	
 	private final ProductService productService;
--- src/main/java/com/ecom/product/controller/ProductVariantController.java
+++ src/main/java/com/ecom/product/controller/ProductVariantController.java
@@ -3,6 +3,7 @@ package com.ecom.product.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/variant")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductVariantController {
 
 	private final ProductVariantService variantService;
